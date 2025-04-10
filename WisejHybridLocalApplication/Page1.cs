01c640c21686b0b3aa23ac923afea67e9f7cca0e
romee <romyoh@gmail.com>
2025-04-10 16:01:44 +0900

    초기 커밋.

---
+++ WisejHybridLocalApplication/Page1.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Linq;
+using Wisej.Hybrid;
+using Wisej.Hybrid.DocumentScanner;
+using Wisej.Web;
+
+namespace WisejHybridLocalApplication
+{
+	public partial class Page1 : Page
+	{
+		public Page1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, System.EventArgs e)
+		{
+			this.pictureBox1.Image = Device.Use<DeviceDocumentScanner>().Scan(1280, 720).FirstOrDefault();
+		}
+	}
+}
+ 
